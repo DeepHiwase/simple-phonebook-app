9ff3d35c7711247180a0eb1cda464617df36553d
Deep <diphiwase0987@gmail.com>
2024-12-13 11:48:48 +0530

    uibuild

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const morgan = require('morgan')
 const morgon = require('morgan')
 const cors = require('cors')
+const Person = require('./models/person')
 const app = express()
 
 let persons = [
@@ -40,7 +42,9 @@ morgon.token('reqBody', (req) => {
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 
 app.get('/api/persons', (req, res) => {
-  res.json(persons)
+  Person.find({}).then(persons => {
+    res.json(persons)
+  })
 })
 
 app.get('/info', (req, res) => {
@@ -99,7 +103,7 @@ app.post('/api/persons', (req, res) => {
   res.json(person)
 })
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   
